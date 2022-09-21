32aa03f395fa5da3d602bfba64cc03fb8a3f1064
Utsav <utsavmp51198@gmail.com>
2022-09-21 12:16:29 +0530

    add cart page in project

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import { Store } from '../../utils/store';
 
 const ProductScreen = () => {
+  const router = useRouter();
   const { state, dispatch } = useContext(Store);
   const { query } = useRouter();
   const { slug } = query;
@@ -25,6 +26,7 @@ const ProductScreen = () => {
     }
 
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
   };
   return (
     <Layout title={product.name}>
