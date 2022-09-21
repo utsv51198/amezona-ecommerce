c301a7977873378fb8fcc4f634b95f1c70ca48df
Utsav <utsavmp51198@gmail.com>
2022-09-21 12:28:14 +0530

    add cart page

--- pages/cart.js
+++ pages/cart.js
@@ -16,6 +16,10 @@ const CartScreen = () => {
   const removeItemHandler = (item) => {
     dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
   };
+  const updateCartHandler = (item, qty) => {
+    const quantity = Number(qty);
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantity } });
+  };
 
   return (
     <Layout title="Shooping Cart">
@@ -59,7 +63,25 @@ const CartScreen = () => {
                         </a>
                       </Link>
                     </td>
-                    <td className="p-5 text-right">{item.quantity}</td>
+                    {/* <td className="p-5 text-right">{item.quantity}</td> */}
+                    <td className="p-5 text-right">
+                      <select
+                        value={item.quantity}
+                        onChange={(e) =>
+                          updateCartHandler(item, e.target.value)
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option
+                            key={x + 1}
+                            value={x + 1}
+                          >
+                            {x + 1}
+                          </option>
+                        ))}
+                      </select>
+                    </td>
+
                     <td className="p-5 text-right">${item.price}</td>
                     <td className="p-5 text-center">
                       <button onClick={() => removeItemHandler(item)}>
